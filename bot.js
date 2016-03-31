dbf6090d4dccc5f3f1571c64b8d38b726d842b71
moskowite <tmoskowite+github@gmail.com>
2016-03-31 02:18:38 +0000

    more functions for the bot

--- bot.js
+++ bot.js
@@ -6,11 +6,54 @@
 var Discord = require("discord.js");
 // Email and Password of discordbot
 var Login = require("./auth.json");
+// Maid-chan json from seroz
+//var maidchan = require("./maidchan.json");
+
 var discordbot = new Discord.Client();
 
-discordbot.on("message", function(message){
-    if(message.content === "ping")
-        discordbot.reply(message, "pong");
+
+//when the bot is ready
+discordbot.on("ready", function () {
+	console.log("Ready to begin! Serving in " + discordbot.channels.length + " channels");
+});
+
+//when the bot disconnects
+discordbot.on("disconnected", function () {
+	//alert the console
+	console.log("Disconnected!");
+
+	//exit node.js with an error
+	process.exit(1);
+});
+
+discordbot.on("message", function(msg){
+    	if (!msg.content.startsWith("$")) return;
+
+	msg.content = msg.content.substr(1);
+
+	if (msg.content === "stats") {
+		msg.reply([
+			"I am connected/have access to:",
+			`${discordbot.servers.length} servers`,
+			`${discordbot.channels.length} channels`,
+			`${discordbot.users.length} users`,
+		].join("\n"));
+	}
+
+	if (msg.content.startsWith("startplaying")) {
+		var game = msg.content.split(" ").slice(1).join(" ");
+		discordbot.setPlayingGame(game);
+	}
+
+	if (msg.content.startsWith("setname") && loose) {
+		discordbot.setUsername(msg.content.split(" ").slice(1).join(" ")).then(() => {
+			msg.reply("done!");
+		});
+	}		
+
+
+	if(msg.content.startsWith("ping"))
+        	discordbot.reply(msg, "really?");
 });
 
 discordbot.login(Login.email, Login.password);
